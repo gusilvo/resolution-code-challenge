ce9e1305e35d6db1dbe4e8856434167ff1ba883b
gusilvo <gustavossilva13@gmail.com>
2024-06-13 14:31:06 -0300

    Update Step2.py

--- Airflow/Steps_and_Others/Step2.py
+++ Airflow/Steps_and_Others/Step2.py
@@ -22,40 +22,41 @@ def extract_and_load_final(meltano_directory):
         # Run Meltano pipeline tap-csv target-postgres
         subprocess.run(['meltano', 'run', 'tap-csv', 'target-postgres'])
 
+# Define the working directory
+meltano_directory = r'C:\Users\User\Meltano\'
 
 tables = tables_files = [
     {'table_name': 'categories',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\categories_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\categories_file.json'},
     {'table_name': 'customer_customer_demo',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\customer_customer_demo_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\customer_customer_demo_file.json'},
     {'table_name': 'customer_demographics',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\customer_demographics_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\customer_demographics_file.json'},
     {'table_name': 'customers',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\customers_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\customers_file.json'},
     {'table_name': 'employee_territories',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\employee_territories_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\employee_territories_file.json'},
     {'table_name': 'employees',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\employees_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\employees_file.json'},
     {'table_name': 'orders',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\orders_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\orders_file.json'},
     {'table_name': 'products',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\products_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\products_file.json'},
     {'table_name': 'region',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\region_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\region_file.json'},
     {'table_name': 'shippers',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\shippers_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\shippers_file.json'},
     {'table_name': 'suppliers',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\suppliers_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\suppliers_file.json'},
     {'table_name': 'territories',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\territories_file.json'},
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\territories_file.json'},
     {'table_name': 'us_states',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\us_states_file.json'},
+     'file_path': rf'{meltano directory}t\extract\tap-csv\final_extract\us_states_file.json'},
     {'table_name': 'order_details',
-     'file_path': r'C:\Users\Meltano\my-project\extract\tap-csv\final_extract\order_details_file.json'}
+     'file_path': rf'{meltano directory}\extract\tap-csv\final_extract\order_details_file.json'}
 ]
 
 
 # Call function to configure Meltano and execute pipeline
 if __name__ == "__main__":
-    meltano_directory = r'C:\Users\gustavo\Desktop\Meltano\my-project'
     extract_and_load_final(meltano_directory)
